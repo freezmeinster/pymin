89258199f077382031e1771b9ab889bf0061a0b4
freez_meinster <freez_meinster@yahoo.co.id>
2011-01-22 13:44:35 +0700

    baru nih

--- controller/Pymin.py
+++ controller/Pymin.py
@@ -1,15 +1,18 @@
-import cherrypy
-from lib import template_loader
+import cherrypy,os
+from lib import template_loader,setting
 
 class controller():
     
     @cherrypy.expose
     def index(self):
-	return "aku pymin"
+	vps = os.listdir('/')
+	data = { 'nama' : 'data' , 'data' : vps }
+	return template_loader.render('coba.html',data)
 	
 
     @cherrypy.expose
     def list_vps(self):
-	data = ['bram','ganteng','sekali',]
+	nguk = ['bram','huh']
+	data = { 'nama' : 'data' , 'data' : nguk }
 	return template_loader.render('coba.html',data)
 	
